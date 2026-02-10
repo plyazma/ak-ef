01636f56a49c0882608d3cab5eef7b4a68612aa9
plyazma <konzeliz21@gmail.com>
2026-02-10 08:45:21 +0200

    Clean Start

---
+++ script.js
@@ -0,0 +1,12 @@
+const sidebarButtons = document.querySelectorAll(".sidebar-btn");
+const tabContents = document.querySelectorAll(".tab-content");
+
+sidebarButtons.forEach(sidebarButton => {
+    sidebarButton.addEventListener("click", () => {
+        sidebarButtons.forEach(sidebarBtn => sidebarBtn.classList.remove("active"));
+        tabContents.forEach(tabContent => tabContent.classList.remove("active"));
+        sidebarButton.classList.add("active");
+        document.getElementById(sidebarButton.dataset.tab)
+            .classList.add("active");
+    });
+});
