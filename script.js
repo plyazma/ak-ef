1c9f70a5ba7a28269788e0ed510adaf4a5dbe577
plyazma <konzeliz21@gmail.com>
2026-02-13 23:18:29 +0200

    -

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Tab Navigation
+// Navigation
 const sidebarButtons = document.querySelectorAll(".sidebar-btn");
 const tabContents = document.querySelectorAll(".tab-content");
 sidebarButtons.forEach(sidebarButton => {
@@ -11,44 +11,96 @@ sidebarButtons.forEach(sidebarButton => {
     });
 });
 
-// Tab 1
+// Calculations
+const path = {
+    valley4: {
+        stations: ["refugee_camp", "infra_station", "reconstruction_hq"],
+        products: ["buck_capsule_a", "hc_valley_battery", "sc_valley_battery", "buck_capsule_b", "buck_capsule_c", "amethyst_bottle", "amethyst_part", "origocrust", "lc_valley_battery", "ferrium_part", "steel_part"]
+    },
+    wuling: {
+        stations: ["sky_king_flats_construction_site"],
+        products: ["lc_wuling_battery", "yazhen_syringe_c", "xiranite"]
+    }
+};
 
-function getNum(id) {
-    const isNum = document.getElementById(id);
-    return Number(isNum.value ?? isNum.textContent) || 0;
+const batteryType = {
+    valley4: ["valley4_originium_ore", "lc_valley_battery", "sc_valley_battery", "hc_valley_battery"],
+    wuling: ["wuling_originium_ore", "lc_wuling_battery"]
 }
 
-function updateStation(prefix) {
-    document.getElementById(`${prefix}-earning_estimates`).textContent =
-        getNum(`${prefix}-earning_efficiency`) * (100 + getNum(`${prefix}-bonus_earning_chance`)) / 100;
-}
+function update() {
+    Object.keys(path).forEach(region => {
+        // Region Gain Estimates
+        let regionEstimates = 0;
+        path[region].stations.forEach(station => {
+            const earningEfficiency = Number(document.getElementById(`${region}-${station}-earning_efficiency`).textContent) || 0;
+            const bonusEarningChance = Number(document.getElementById(`${region}-${station}-bonus_earning_chance`).value) || 0;
+            const stationEstimates = earningEfficiency * (1 + bonusEarningChance / 100);
+            document.getElementById(`${region}-${station}-earning_estimates`).textContent = stationEstimates.toFixed(0);
+            regionEstimates += stationEstimates;
+        });
+        document.getElementById(`${region}-earning_estimates`).textContent = regionEstimates.toFixed(0);
 
-const regions = {
-    "valley4_ov": ["refugee_camp", "infra_station", "reconstruction_hq"],
-    "wuling_ov": ["sky_king_flats_construction_site"]
-}
+        // Stock Bill Coverage
+        let stockBillCoverage = 0;
+        path[region].products.forEach(product => {
+            const factoriesNum = Number(document.getElementById(`${region}-${product}-factories_num`).value) || 0;
+            const _1Output = Number(document.getElementById(`${region}-${product}-1_output`).textContent) || 0;
+            const productOutput = factoriesNum * _1Output;
+            document.getElementById(`${region}-${product}-output`).textContent = productOutput;
 
-function updateRegion(regionId) {
-    regions[regionId].forEach(updateStation);
-    document.getElementById(`${regionId}-earning_estimates`).textContent =
-        regions[regionId].reduce((sum, p) => sum + getNum(`${p}-earning_estimates`), 0);
+            const price = Number(document.getElementById(`${region}-${product}-price`).textContent) || 0;
+            const stockBillIncomePerMinute = factoriesNum * price * _1Output;
+            document.getElementById(`${region}-${product}-income_per_minute`).textContent = stockBillIncomePerMinute;
+
+            const stockBillIncomePerHour = stockBillIncomePerMinute * 60;
+            document.getElementById(`${region}-${product}-income_per_hour`).textContent = stockBillIncomePerHour;
+            stockBillCoverage += stockBillIncomePerHour;
+        });
+        document.getElementById(`${region}-coverage`).textContent = stockBillCoverage;
+        document.getElementById(`${region}-percent_covered`).textContent = regionEstimates
+            ? (stockBillCoverage / regionEstimates * 100).toFixed(2)
+            : 0;
+
+        // Ore Consumption
+        let regionFactoriesNum = 0;
+        path[region].products.forEach(product => {
+            const factoriesNum = Number(document.getElementById(`${region}-${product}-factories_num`).value) || 0;
+            regionFactoriesNum += factoriesNum;
+
+            const _1CostOriginium = Number(document.getElementById(`${region}-${product}-1_cost-originium`).textContent);
+            const _1CostAmethyst = Number(document.getElementById(`${region}-${product}-1_cost-amethyst`).textContent);
+            const _1CostFerrium = Number(document.getElementById(`${region}-${product}-1_cost-ferrium`).textContent);
+            const costOriginium = _1CostOriginium * factoriesNum;
+            const costAmethyst = _1CostAmethyst * factoriesNum;
+            const costFerrium = _1CostFerrium * factoriesNum;
+            document.getElementById(`${region}-${product}-n_cost-originium`).textContent = costOriginium;
+            document.getElementById(`${region}-${product}-n_cost-amethyst`).textContent = costAmethyst;
+            document.getElementById(`${region}-${product}-n_cost-ferrium`).textContent = costFerrium;
+        });
+        document.getElementById(`${region}-factories_num`).textContent = regionFactoriesNum;
+
+        // Battery Consumption
+        Object.keys(batteryType).forEach(batteryRegion => {
+            batteryType[batteryRegion].forEach(batteryProduct => {
+                if (
+                    !document.getElementById(`${batteryRegion}-${batteryProduct}-consumption`) ||
+                    !document.getElementById(`${batteryRegion}-${batteryProduct}-output`)
+                ) return;
+
+                const thermalBankCount = Number(document.getElementById(`${batteryRegion}-${batteryProduct}-thermal_bank_count`).value);
+                const consumptionSpeed = Number(document.getElementById(`${batteryRegion}-${batteryProduct}-consumption_speed`).textContent);
+                const consumption = thermalBankCount * consumptionSpeed;
+                const output = Number(document.getElementById(`${batteryRegion}-${batteryProduct}-output`).textContent) || 0;
+                document.getElementById(`${batteryRegion}-${batteryProduct}-consumption`).textContent = consumption;
+                document.getElementById(`${batteryRegion}-${batteryProduct}-output`).textContent = output - consumption;
+            });
+        });
+    });
 }
 
 document.querySelectorAll("input").forEach(input => {
-    input.addEventListener("input", function() {
-        const prefix = this.id.split("-")[0];
-
-        for (const regionId in regions) {
-            if (regions[regionId].includes(prefix)) {
-                updateRegion(regionId);
-                break;
-            }
-        }
-    });
+    input.addEventListener("input", update);
 });
 
-for (const regionId in regions) updateRegion(regionId)
-
-// Tab 2 // Valley IV
-
-function updateFactory
+update();
